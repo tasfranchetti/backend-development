a8c22dbbe3a688e9d1df12b64b8e6afb75df8b0a
tasfranchetti <franchettitasiana@gmail.com>
2022-07-14 20:17:45 -0300

    test-package

--- webServer/express/server.js
+++ webServer/express/server.js
@@ -1,36 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 8080; //if "0", the port num is random
 
-const Container = require('./script')
-const productos = new Container('products.txt')
+const Container = require('./script');
+const productos = new Container('products.txt');
 
 //error handling 
 app.on("error", error => {
-    console.log(`Error on the server: ${error}`)
+    console.log(`Error on the server: ${error}`);
 })
 
 const server = app.listen(PORT, () =>{
-    console.log(`Server ready: ${server.address().port}`)
+    console.log(`Server ready: ${server.address().port}`);
 })  
 
 //GET request - this is one route and I can have many
 app.get('/', (req, res) => {
-    res.send(`Visit /products or /randomProduct`)
+    res.send(`Visit /products or /randomProduct`);
 })
 
 app.get('/products', async (req, res) => {
-    await productos.getAll()
-    .then(
-        products => res.send(products)
-        )
+    products = await productos.getAll();
+    res.send(products);
 })
 
 app.get('/randomProduct', async (req, res) => {
-    await productos.getRandom()
-    .then(
-        product => res.send(product)
-    )
+    product = await productos.getRandom();
+    res.send(product);
 })
 
 
